812edf76328c67933770d76f2ee7050f8446be9c
Justin Myers <42520471+Devman0451@users.noreply.github.com>
2019-06-07 18:30:53 -0400

    Algorithms

---
+++ arcade-intro/areEquallyStrong-19.php
@@ -0,0 +1,5 @@
+<?php
+
+function areEquallyStrong($yourLeft, $yourRight, $friendsLeft, $friendsRight) {
+    return (($yourLeft + $yourRight) == ($friendsLeft + $friendsRight)) && ($yourLeft == $friendsLeft || $yourLeft == $friendsRight);
+}
