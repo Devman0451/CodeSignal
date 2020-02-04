24a7035ba697202980497b6f3402a5836e966ebf
Justin Myers <42520471+Devman0451@users.noreply.github.com>
2020-02-04 18:27:55 -0500

    SQL

---
+++ databases/db-16.sql
@@ -0,0 +1,12 @@
+CREATE PROCEDURE newsSubscribers()
+    SELECT DISTINCT subscriber
+    FROM
+    (
+        SELECT *
+        FROM full_year
+            UNION
+        SELECT *
+        FROM half_year
+    ) x
+    WHERE newspaper LIKE "%Daily%"
+    ORDER BY subscriber ASC;
